996b005f90095452784afca1a6bdedbceaab06c7
ajedrych <jedrych.al@gmail.com>
2021-05-28 14:26:12 +0200

    filter + new layout

--- app.R
+++ app.R
@@ -1,64 +1,101 @@
+#Loading libraries
 library(shiny)
 library(tidyr)
 library(ggplot2)
 library(DT)
+library(RSQLite)
+library(dbplyr)
+library(dplyr)
+library(ggvis)
 
+#Loading data from URL
 load(url("https://stat.duke.edu/~mc301/data/movies.Rdata"))
 
+#Removing rows with missing values from table
+drop_na(data = movies)
+
+#User interface
 ui <- fluidPage(
-  titlePanel("Movies ratings analysis"),
 
-  sidebarLayout(
+  #Title
+  titlePanel("Analysis of movie ratings"),
 
-    sidebarPanel(
+  #Creating tabs
+  tabsetPanel(
 
-      selectInput("title_type", "Type",
-        c("Documentary", "Feature Film", "TV Movie")),
+          #Tab1 - displaying scatterplot with reactive elements
+           tabPanel("Plot",
+                    sidebarLayout(
+                      sidebarPanel(
 
-      selectInput("genre", "Genre",
-          c("All", "Action", "Adventure", "Animation", "Biography", "Comedy",
-            "Crime", "Documentary", "Drama", "Family", "Fantasy", "History",
-            "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi",
-            "Short", "Sport", "Thriller", "War", "Western")),
+                        selectInput("genre", "Genre",
+                                    c("All", "Action", "Adventure", "Animation", "Biography", "Comedy", "Crime", "Documentary", "Drama", "Family", "Fantasy", "History", "Horror", "Music", "Musical", "Mystery", "Romance", "Sci-Fi", "Short", "Sport", "Thriller", "War", "Western")),
 
-      sliderInput("thtr_rel_year", "Year released", 1970, 2014, value = c(1970,2014), step = 1),
+                        sliderInput("thtr_rel_year", "Release year", 1970, 2014, value = c(1970,2014), step = 1, round = TRUE, sep=""),
 
-      sliderInput("runtime", "Length",65,267, value = c(65,267), step = 1),
+                        sliderInput("runtime", "Movie length",65,267, value = c(65,267), step = 1, round = TRUE, sep=""),
 
-      selectInput("x", "X-axis",
-        c("runtime", "imdb_rating", "imdb_num_votes", "critics_score", "audience_score"),
-        selected = "runtime"),
+                        selectInput("x", "X-axis",
+                                    c("runtime", "imdb_rating", "imdb_num_votes", "critics_score", "audience_score"),
+                                    selected = "runtime"),
 
-      selectInput("y", "Y-axis",
-        c("runtime", "imdb_rating", "imdb_num_votes", "critics_score", "audience_score"),
-        selected = "imdb_rating")
-    ),
+                        selectInput("y", "Y-axis",
+                                    c("runtime", "imdb_rating", "imdb_num_votes", "critics_score", "audience_score"),
+                                    selected = "imdb_rating")
+                      ),
 
-    # Output --------------------------------------------------------
-    mainPanel(
-      tabsetPanel(
-            tabPanel('Plot', plotOutput("scatterplot"),
-            tabPanel( "Table", dataTableOutput("moviestable"))
-        )
-    )
+                      mainPanel(plotOutput("scatterplot"),
+                                span("Number of movies selected:", textOutput("n_movies"))
+                      )
+                    )
+           ),
+
+           #Tab2 - dispalying data table
+           tabPanel("Data", dataTableOutput("moviestable"))
+
   )
 )
-)
+
 # Define server function required to create the scatterplot ---------
 server <- function(input, output) {
 
+  movies_a <- reactive({
+    minyear <- input$thtr_rel_year[1]
+    maxyear <- input$thtr_rel_year[2]
+    minruntime <- input$runtime[1]
+    maxruntime <- input$runtime[2]
+
   # Create scatterplot object the plotOutput function is expecting --
   output$scatterplot <- renderPlot({
-    ggplot(data = movies, aes_string(x = input$x, y = input$y)) +
+    ggplot(m, aes_string(x = input$x, y = input$y)) +
     geom_point()
   })
 
-  # Print data table if checked -------------------------------------
+  m <- movies %>%
+      filter(
+        thtr_rel_year >= minyear,
+        thtr_rel_year <= maxyear,
+        runtime >= minruntime,
+        runtime <= maxruntime
+      ) %>%
+    arrange(runtime)
+
+    if (input$genre != "All") {
+      genre <- paste0(input$genre)
+      m <- m %>% filter(genre %like% genre)
+    }
+
+    m <- as.data.frame(m)
+ })
+
+  # Print data table if checked -------------------------------------)
   output$moviestable <- renderDataTable({
-    datatable(data = movies[, 1:4],
+    datatable(data = movies_table,
                   options = list(pageLength = 10),
                   rownames = FALSE)
   })
+
+   output$n_movies <- renderText({ nrow(movies_a()) })
 }
 
 shinyApp(ui = ui, server = server)
